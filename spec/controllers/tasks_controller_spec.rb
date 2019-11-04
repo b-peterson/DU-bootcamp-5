1485589c2ae4765e1c52751f6c31eb26056c1b63
Benjamin Peterson <petersonbenjamint@gmail.com>
2019-11-04 03:16:29 +0000

    Added test and controller changes to add task

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-    describe "tasks@index" do
+    describe "tasks#index" do
         it "should list the tasks in the database" do
             task1 = FactoryBot.create(:task)
             task2 = FactoryBot.create(:task)
@@ -24,6 +24,16 @@ RSpec.describe TasksController, type: :controller do
             expect(response).to have_http_status(:success)
             task.reload
             expect(task.done).to eq(true)
+        end
+    end
+
+    describe "task#create" do
+        it "should allow a new task to be created" do
+            post :create, params: {task: {title: "Fix things"}}
+            expect(response).to have_http_status(:success)
+            response_value = ActiveSupport::JSON.decode(@response.body)
+            expect(response_value['title']).to eq("Fix things")
+            expect(Task.last.title).to eq("Fix things")
         end
     end
 end
